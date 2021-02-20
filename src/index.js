4c6ac004df00aa4f32197d62164e0c91f6fcbdc9
William Thompson <Thompsonwilliam729@gmail.com>
2021-02-20 13:34:36 -0800

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-
+//
 window.store = store
 ReactDOM.render(
   <Provider store={store}>
